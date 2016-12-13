554d39c6013483c4fa3b561a91e211d51995b0fa
179309463 <179309463@qq.com>
2016-12-13 21:52:20 +0800

    remove controller.js from components

--- src/app/theme/components/baSidebar/component.js
+++ src/app/theme/components/baSidebar/component.js
@@ -14,7 +14,23 @@
     return {
       restrict: 'E',
       templateUrl: 'app/theme/components/baSidebar/template.html',
-      controller: 'BaSidebarCtrl',
+      controller: function($scope, baSidebarService) {
+        $scope.menuItems = baSidebarService.getMenuItems();
+        $scope.defaultSidebarState = $scope.menuItems[0].stateRef;
+
+        $scope.hoverItem = function ($event) {
+          $scope.showHoverElem = true;
+          $scope.hoverElemHeight =  $event.currentTarget.clientHeight;
+          var menuTopValue = 66;
+          $scope.hoverElemTop = $event.currentTarget.getBoundingClientRect().top - menuTopValue;
+        };
+
+        $scope.$on('$stateChangeSuccess', function () {
+          if (baSidebarService.canSidebarBeHidden()) {
+            baSidebarService.setMenuCollapsed(true);
+          }
+        });
+      },
       link: function(scope, el) {
 
         scope.menuHeight = el[0].childNodes[0].clientHeight - 84;
